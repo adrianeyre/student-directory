b6668b12032ba12b32228db30eaba931090d78e1
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-12-05 10:54:14 +0000

    Uptp task 8

--- directory.rb
+++ directory.rb
@@ -7,11 +7,22 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repat this code
   while !name.empty? do
+    puts "Enter cohort: "
+    cohort = gets.chomp
+    cohort.gsub!(" ","")
+    cohort = "november" if cohort == ""
+    cohort = cohort.to_sym
+    puts "Enter hobbies: "
+    hobbies = gets.chomp
+    puts "Enter Country of Birth: "
+    country = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, hobbies: hobbies, country: country}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Enter another name or hit return to finish: "
     name = gets.chomp
+    p cohort
   end
   # return the array of students
   students
@@ -25,9 +36,21 @@ end
 
 def print(students)
   # iteration around the students array
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student,index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort, hobbies=#{student[:hobbies]}, Country of Birth=#{student[:country]})"
   end
+  puts
+end
+
+def print_while(students)
+  # iteration around the students array
+  puts "Displaying students using a WHILE"
+  index = 0
+  while index < students.count do
+    puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    index += 1
+  end
+  puts
 end
 
 def print_footer(students)
@@ -35,8 +58,49 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def display_by_letter(students,letter)
+  # iteration around the students array
+  puts "Displaying students that begin with the letter #{letter}"
+  students.each_with_index do |student,index|
+    if letter.downcase == student[:name][0]
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+  puts
+end
+
+def display_less_twelve(students)
+  # iteration around the students array
+  puts "Displaying students that are less than 12 in length"
+  students.each_with_index do |student,index|
+    if student[:name].length < 12
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+  puts
+end
+
+def align_students(students)
+  # align the student names
+  cen = 0
+  # work out which has the longest name
+  students.each do |name|
+    cen = name[:name].length if name[:name].length > cen
+  end
+  cen += 4
+  puts "Centre".center(cen)
+  # Centre the names
+  students.each do |name|
+    puts name[:name].center(cen)
+  end
+end
 # Nothing happens until we call the methods
 students = input_students
 print_header
 print(students)
 print_footer(students)
+puts
+display_by_letter(students,"a")
+display_less_twelve(students)
+print_while(students)
+align_students(students)
